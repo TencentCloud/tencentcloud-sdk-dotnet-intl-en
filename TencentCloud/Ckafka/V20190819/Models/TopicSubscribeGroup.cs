518f98ee251b3d0bfb5fea978a3968434c1e7b47
tencentcloudapi <tencentcloudapi@tencent.com>
2021-09-02 08:26:26 +0800

    release 3.0.271

---
+++ TencentCloud/Ckafka/V20190819/Models/TopicSubscribeGroup.cs
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 THL A29 Limited, a Tencent company. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace TencentCloud.Ckafka.V20190819.Models
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using TencentCloud.Common;
+
+    public class TopicSubscribeGroup : AbstractModel
+    {
+        
+        /// <summary>
+        /// Total number
+        /// </summary>
+        [JsonProperty("TotalCount")]
+        public long? TotalCount{ get; set; }
+
+        /// <summary>
+        /// Number of consumer group status
+        /// </summary>
+        [JsonProperty("StatusCountInfo")]
+        public string StatusCountInfo{ get; set; }
+
+        /// <summary>
+        /// Consumer group information
+        /// Note: this field may return `null`, indicating that no valid values can be obtained.
+        /// </summary>
+        [JsonProperty("GroupsInfo")]
+        public GroupInfoResponse[] GroupsInfo{ get; set; }
+
+        /// <summary>
+        /// Whether a request is asynchronous. If there are fewer consumer groups in the instances, the result will be returned directly, and status code is 1. When there are many consumer groups in the instances, cache will be updated asynchronously. When status code is 0, grouping information will not be returned until cache update is completed and status code becomes 1.
+        /// Note: this field may return `null`, indicating that no valid values can be obtained.
+        /// </summary>
+        [JsonProperty("Status")]
+        public long? Status{ get; set; }
+
+
+        /// <summary>
+        /// For internal usage only. DO NOT USE IT.
+        /// </summary>
+        public override void ToMap(Dictionary<string, string> map, string prefix)
+        {
+            this.SetParamSimple(map, prefix + "TotalCount", this.TotalCount);
+            this.SetParamSimple(map, prefix + "StatusCountInfo", this.StatusCountInfo);
+            this.SetParamArrayObj(map, prefix + "GroupsInfo.", this.GroupsInfo);
+            this.SetParamSimple(map, prefix + "Status", this.Status);
+        }
+    }
+}
+
