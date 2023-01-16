92d9709096ea39aa029d1b2d13ad1b8a41d4141e
tencentcloudapi <tencentcloudapi@tencent.com>
2023-01-16 11:35:22 +0800

    release 3.0.608

--- TencentCloud/Sms/V20210111/Models/DescribeSmsTemplateListRequest.cs
+++ TencentCloud/Sms/V20210111/Models/DescribeSmsTemplateListRequest.cs
@@ -33,8 +33,8 @@ namespace TencentCloud.Sms.V20210111.Models
         public ulong? International{ get; set; }
 
         /// <summary>
-        /// Array of template IDs. If the array is empty, the template list information will be queried by default (only allowed for root accounts). You need to use the `Limit` and `Offset` fields to set the query range.
-        /// <dx-alert infotype="notice" title="Note">The default array length can be up to 100</dx-alert>
+        /// Array of template IDs. If the array is empty, the template list information will be queried by default. You need to use the `Limit` and `Offset` fields to set the query range.
+        /// <dx-alert infotype="notice" title="Note">The max array length is 100 by default.</dx-alert>
         /// </summary>
         [JsonProperty("TemplateIdSet")]
         public ulong?[] TemplateIdSet{ get; set; }
