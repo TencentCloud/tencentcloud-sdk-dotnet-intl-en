34b2d9896d10c591d6b73f6c43f44a8f132dccec
tencentcloudapi <tencentcloudapi@tencent.com>
2024-03-07 05:40:35 +0800

    release 3.0.852

--- TencentCloud/Sqlserver/V20180328/SqlserverClient.cs
+++ TencentCloud/Sqlserver/V20180328/SqlserverClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Sqlserver.V20180328
 
        private const string endpoint = "sqlserver.tencentcloudapi.com";
        private const string version = "2018-03-28";
-       private const string sdkVersion = "SDK_NET_3.0.851";
+       private const string sdkVersion = "SDK_NET_3.0.852";
 
         /// <summary>
         /// Client constructor.
