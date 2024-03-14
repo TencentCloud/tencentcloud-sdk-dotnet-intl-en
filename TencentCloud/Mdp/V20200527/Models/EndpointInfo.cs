fe0d0cad1aec6417ab4cc9e99d4da2ca700239f6
tencentcloudapi <tencentcloudapi@tencent.com>
2024-03-15 05:20:20 +0800

    release 3.0.858

--- TencentCloud/Mdp/V20200527/Models/EndpointInfo.cs
+++ TencentCloud/Mdp/V20200527/Models/EndpointInfo.cs
@@ -42,6 +42,55 @@ namespace TencentCloud.Mdp.V20200527.Models
         [JsonProperty("AuthInfo")]
         public EndpointAuthInfo AuthInfo{ get; set; }
 
+        /// <summary>
+        /// Endpoint protocol.
+        /// </summary>
+        [JsonProperty("Protocol")]
+        public string Protocol{ get; set; }
+
+        /// <summary>
+        /// Manifest name, default is main.
+        /// </summary>
+        [JsonProperty("Manifest")]
+        public string Manifest{ get; set; }
+
+        /// <summary>
+        /// Whether to turn on the time shift function, true: on, false: off, the default is off.
+        /// </summary>
+        [JsonProperty("TimeShiftEnable")]
+        public bool? TimeShiftEnable{ get; set; }
+
+        /// <summary>
+        /// The number of days in the time shift window, up to 30 days. Valid when TimeShiftEnable is turned on.
+        /// </summary>
+        [JsonProperty("TimeShiftDuration")]
+        public ulong? TimeShiftDuration{ get; set; }
+
+        /// <summary>
+        /// Advertising insertion function switch.
+        /// </summary>
+        [JsonProperty("SSAIEnable")]
+        public bool? SSAIEnable{ get; set; }
+
+        /// <summary>
+        /// Ad insertion function configuration information.
+        /// </summary>
+        [JsonProperty("SSAIInfo")]
+        public SSAIConf SSAIInfo{ get; set; }
+
+        /// <summary>
+        /// The customer-defined url parameter is inserted into the subscript at the specified position of the Endpoint url. The optional range of the subscript is: [0,3].
+        /// </summary>
+        [JsonProperty("CustomUrlParamIndex")]
+        public ulong? CustomUrlParamIndex{ get; set; }
+
+        /// <summary>
+        /// Customer-defined url parameters are inserted into the specified position of the Endpoint url based on the CustomUrlParamIndex.
+        /// The parameters can only contain digits, letters, underscores (_), and hyphens (-), with a length of 1 to 64 chars.
+        /// </summary>
+        [JsonProperty("CustomUrlParam")]
+        public string CustomUrlParam{ get; set; }
+
 
         /// <summary>
         /// For internal usage only. DO NOT USE IT.
@@ -51,6 +100,14 @@ namespace TencentCloud.Mdp.V20200527.Models
             this.SetParamSimple(map, prefix + "Name", this.Name);
             this.SetParamSimple(map, prefix + "Url", this.Url);
             this.SetParamObj(map, prefix + "AuthInfo.", this.AuthInfo);
+            this.SetParamSimple(map, prefix + "Protocol", this.Protocol);
+            this.SetParamSimple(map, prefix + "Manifest", this.Manifest);
+            this.SetParamSimple(map, prefix + "TimeShiftEnable", this.TimeShiftEnable);
+            this.SetParamSimple(map, prefix + "TimeShiftDuration", this.TimeShiftDuration);
+            this.SetParamSimple(map, prefix + "SSAIEnable", this.SSAIEnable);
+            this.SetParamObj(map, prefix + "SSAIInfo.", this.SSAIInfo);
+            this.SetParamSimple(map, prefix + "CustomUrlParamIndex", this.CustomUrlParamIndex);
+            this.SetParamSimple(map, prefix + "CustomUrlParam", this.CustomUrlParam);
         }
     }
 }
---
+++ TencentCloud/Mdp/V20200527/Models/SSAIConf.cs
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2018 THL A29 Limited, a Tencent company. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace TencentCloud.Mdp.V20200527.Models
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using TencentCloud.Common;
+
+    public class SSAIConf : AbstractModel
+    {
+        
+        /// <summary>
+        /// Advertising Decision Server URL (ADS).
+        /// </summary>
+        [JsonProperty("AdsUrl")]
+        public string AdsUrl{ get; set; }
+
+        /// <summary>
+        /// Parameter configuration.
+        /// </summary>
+        [JsonProperty("ConfigAliases")]
+        public ConfigAliasesInfo[] ConfigAliases{ get; set; }
+
+        /// <summary>
+        /// Default advertising url.
+        /// </summary>
+        [JsonProperty("SlateAd")]
+        public string SlateAd{ get; set; }
+
+        /// <summary>
+        /// Maximum unfilled duration, unit: seconds.
+        /// </summary>
+        [JsonProperty("Threshold")]
+        public ulong? Threshold{ get; set; }
+
+        /// <summary>
+        /// Whether to enable mpd location, true corresponds to enable, false corresponds to disable.
+        /// </summary>
+        [JsonProperty("DashMPDLocation")]
+        public bool? DashMPDLocation{ get; set; }
+
+        /// <summary>
+        /// Whether to enable transparent transmission of advertising tags.
+        /// </summary>
+        [JsonProperty("AdMarkerPassthrough")]
+        public bool? AdMarkerPassthrough{ get; set; }
+
+        /// <summary>
+        /// How to process tags in advertisements, optional values: [1,2] 
+        /// 1: Process all SCTE-35 type tags - all (default) 
+        /// 2: SCTE-35enhanced, parse some types.
+        /// </summary>
+        [JsonProperty("SCTE35AdType")]
+        public ulong? SCTE35AdType{ get; set; }
+
+        /// <summary>
+        /// The type of tag that is regarded as an advertisement, optional values: [1,8]
+        /// 1. Splice insert 
+        /// 2. Provider advertisement 
+        /// 3. Distributor advertisement 
+        /// 4. Provider placement opportunity 
+        /// 5. Distributor placement opportunity 
+        /// 6. Break 
+        /// 7. Provider overlay placement opportunity 
+        /// 8. Distributor overlay placement opportunity.
+        /// </summary>
+        [JsonProperty("AdTriggers")]
+        public ulong?[] AdTriggers{ get; set; }
+
+        /// <summary>
+        /// The type of distribution restriction that is considered an advertisement, optional values: [1,4]
+        /// 1:None 
+        /// 2:Restricted (default) 
+        /// 3:Unrestricted 
+        /// 4.Both.
+        /// </summary>
+        [JsonProperty("DeliveryRestrictions")]
+        public ulong? DeliveryRestrictions{ get; set; }
+
+
+        /// <summary>
+        /// For internal usage only. DO NOT USE IT.
+        /// </summary>
+        public override void ToMap(Dictionary<string, string> map, string prefix)
+        {
+            this.SetParamSimple(map, prefix + "AdsUrl", this.AdsUrl);
+            this.SetParamArrayObj(map, prefix + "ConfigAliases.", this.ConfigAliases);
+            this.SetParamSimple(map, prefix + "SlateAd", this.SlateAd);
+            this.SetParamSimple(map, prefix + "Threshold", this.Threshold);
+            this.SetParamSimple(map, prefix + "DashMPDLocation", this.DashMPDLocation);
+            this.SetParamSimple(map, prefix + "AdMarkerPassthrough", this.AdMarkerPassthrough);
+            this.SetParamSimple(map, prefix + "SCTE35AdType", this.SCTE35AdType);
+            this.SetParamArraySimple(map, prefix + "AdTriggers.", this.AdTriggers);
+            this.SetParamSimple(map, prefix + "DeliveryRestrictions", this.DeliveryRestrictions);
+        }
+    }
+}
+
