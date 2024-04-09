5928be739ff669f0f33894540f210717cc6a2fd2
tencentcloudapi <tencentcloudapi@tencent.com>
2024-04-10 05:15:48 +0800

    release 3.0.875

--- TencentCloud/Dts/V20211206/DtsClient.cs
+++ TencentCloud/Dts/V20211206/DtsClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Dts.V20211206
 
        private const string endpoint = "dts.tencentcloudapi.com";
        private const string version = "2021-12-06";
-       private const string sdkVersion = "SDK_NET_3.0.874";
+       private const string sdkVersion = "SDK_NET_3.0.875";
 
         /// <summary>
         /// Client constructor.
