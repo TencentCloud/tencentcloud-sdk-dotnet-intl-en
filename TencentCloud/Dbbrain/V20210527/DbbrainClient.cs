5e49fbe4c470f70b15857853947a2d4b63bc7702
tencentcloudapi <tencentcloudapi@tencent.com>
2024-04-19 05:19:32 +0800

    release 3.0.881

--- TencentCloud/Dbbrain/V20210527/DbbrainClient.cs
+++ TencentCloud/Dbbrain/V20210527/DbbrainClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Dbbrain.V20210527
 
        private const string endpoint = "dbbrain.tencentcloudapi.com";
        private const string version = "2021-05-27";
-       private const string sdkVersion = "SDK_NET_3.0.880";
+       private const string sdkVersion = "SDK_NET_3.0.881";
 
         /// <summary>
         /// Client constructor.
