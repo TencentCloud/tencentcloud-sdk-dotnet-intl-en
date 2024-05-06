c0262606522748903077eba7cef3a0522306dbc8
tencentcloudapi <tencentcloudapi@tencent.com>
2024-05-06 16:35:28 +0800

    release 3.0.889

--- TencentCloud/Cfw/V20190904/CfwClient.cs
+++ TencentCloud/Cfw/V20190904/CfwClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Cfw.V20190904
 
        private const string endpoint = "cfw.tencentcloudapi.com";
        private const string version = "2019-09-04";
-       private const string sdkVersion = "SDK_NET_3.0.888";
+       private const string sdkVersion = "SDK_NET_3.0.889";
 
         /// <summary>
         /// Client constructor.
