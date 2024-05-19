ef365ece62b429796539a42d133eafbc6229cdde
tencentcloudapi <tencentcloudapi@tencent.com>
2024-05-20 05:08:01 +0800

    release 3.0.900

--- TencentCloud/Cam/V20190116/CamClient.cs
+++ TencentCloud/Cam/V20190116/CamClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Cam.V20190116
 
        private const string endpoint = "cam.tencentcloudapi.com";
        private const string version = "2019-01-16";
-       private const string sdkVersion = "SDK_NET_3.0.899";
+       private const string sdkVersion = "SDK_NET_3.0.900";
 
         /// <summary>
         /// Client constructor.
