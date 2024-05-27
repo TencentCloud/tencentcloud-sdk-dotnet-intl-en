ef88b60c5ce10e47a94a1e292883d5710c956808
tencentcloudapi <tencentcloudapi@tencent.com>
2024-05-28 05:06:24 +0800

    release 3.0.906

--- TencentCloud/Kms/V20190118/KmsClient.cs
+++ TencentCloud/Kms/V20190118/KmsClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Kms.V20190118
 
        private const string endpoint = "kms.tencentcloudapi.com";
        private const string version = "2019-01-18";
-       private const string sdkVersion = "SDK_NET_3.0.905";
+       private const string sdkVersion = "SDK_NET_3.0.906";
 
         /// <summary>
         /// Client constructor.
