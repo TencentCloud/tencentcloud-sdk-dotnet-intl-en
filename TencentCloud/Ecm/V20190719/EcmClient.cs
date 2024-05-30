999471677f7a25645c63996bda9927f4e3e7cddb
tencentcloudapi <tencentcloudapi@tencent.com>
2024-05-31 05:18:32 +0800

    release 3.0.909

--- TencentCloud/Ecm/V20190719/EcmClient.cs
+++ TencentCloud/Ecm/V20190719/EcmClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Ecm.V20190719
 
        private const string endpoint = "ecm.tencentcloudapi.com";
        private const string version = "2019-07-19";
-       private const string sdkVersion = "SDK_NET_3.0.908";
+       private const string sdkVersion = "SDK_NET_3.0.909";
 
         /// <summary>
         /// Client constructor.
