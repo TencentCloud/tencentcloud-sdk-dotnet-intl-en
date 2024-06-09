0c78208b468df1f80d335cbce4902e7306073623
tencentcloudapi <tencentcloudapi@tencent.com>
2024-06-10 05:14:50 +0800

    release 3.0.915

--- TencentCloud/Mdp/V20200527/MdpClient.cs
+++ TencentCloud/Mdp/V20200527/MdpClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Mdp.V20200527
 
        private const string endpoint = "mdp.tencentcloudapi.com";
        private const string version = "2020-05-27";
-       private const string sdkVersion = "SDK_NET_3.0.914";
+       private const string sdkVersion = "SDK_NET_3.0.915";
 
         /// <summary>
         /// Client constructor.
