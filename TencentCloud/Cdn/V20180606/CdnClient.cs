60ea790494f7163de4c169f3ff7beadcd55ca6b9
tencentcloudapi <tencentcloudapi@tencent.com>
2024-07-10 05:15:23 +0800

    release 3.0.936

--- TencentCloud/Cdn/V20180606/CdnClient.cs
+++ TencentCloud/Cdn/V20180606/CdnClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Cdn.V20180606
 
        private const string endpoint = "cdn.tencentcloudapi.com";
        private const string version = "2018-06-06";
-       private const string sdkVersion = "SDK_NET_3.0.935";
+       private const string sdkVersion = "SDK_NET_3.0.936";
 
         /// <summary>
         /// Client constructor.
--- TencentCloud/Dbbrain/V20191016/DbbrainClient.cs
+++ TencentCloud/Dbbrain/V20191016/DbbrainClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Dbbrain.V20191016
 
        private const string endpoint = "dbbrain.tencentcloudapi.com";
        private const string version = "2019-10-16";
-       private const string sdkVersion = "SDK_NET_3.0.935";
+       private const string sdkVersion = "SDK_NET_3.0.936";
 
         /// <summary>
         /// Client constructor.
