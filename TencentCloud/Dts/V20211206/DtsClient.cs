656644b9725a2306c9e7778ce0b3a2c8afe8eb15
tencentcloudapi <tencentcloudapi@tencent.com>
2024-07-15 05:06:25 +0800

    release 3.0.939

--- TencentCloud/Dts/V20211206/DtsClient.cs
+++ TencentCloud/Dts/V20211206/DtsClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Dts.V20211206
 
        private const string endpoint = "dts.tencentcloudapi.com";
        private const string version = "2021-12-06";
-       private const string sdkVersion = "SDK_NET_3.0.938";
+       private const string sdkVersion = "SDK_NET_3.0.939";
 
         /// <summary>
         /// Client constructor.
--- TencentCloud/Monitor/V20180724/MonitorClient.cs
+++ TencentCloud/Monitor/V20180724/MonitorClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Monitor.V20180724
 
        private const string endpoint = "monitor.tencentcloudapi.com";
        private const string version = "2018-07-24";
-       private const string sdkVersion = "SDK_NET_3.0.938";
+       private const string sdkVersion = "SDK_NET_3.0.939";
 
         /// <summary>
         /// Client constructor.
