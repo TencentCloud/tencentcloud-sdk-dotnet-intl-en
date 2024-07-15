22d61d0a68e3832f6547b02be7b7f9ab825142f6
tencentcloudapi <tencentcloudapi@tencent.com>
2024-07-16 05:13:33 +0800

    release 3.0.940

--- TencentCloud/Ses/V20201002/SesClient.cs
+++ TencentCloud/Ses/V20201002/SesClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Ses.V20201002
 
        private const string endpoint = "ses.tencentcloudapi.com";
        private const string version = "2020-10-02";
-       private const string sdkVersion = "SDK_NET_3.0.939";
+       private const string sdkVersion = "SDK_NET_3.0.940";
 
         /// <summary>
         /// Client constructor.
--- TencentCloud/Teo/V20220106/TeoClient.cs
+++ TencentCloud/Teo/V20220106/TeoClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Teo.V20220106
 
        private const string endpoint = "teo.tencentcloudapi.com";
        private const string version = "2022-01-06";
-       private const string sdkVersion = "SDK_NET_3.0.939";
+       private const string sdkVersion = "SDK_NET_3.0.940";
 
         /// <summary>
         /// Client constructor.
