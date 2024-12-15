b119c5cec517e24447d90c20c7f4610d001f1408
tencentcloudapi <tencentcloudapi@tencent.com>
2024-12-16 05:06:11 +0800

    release 3.0.1033

--- TencentCloud/Tdmq/V20200217/TdmqClient.cs
+++ TencentCloud/Tdmq/V20200217/TdmqClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Tdmq.V20200217
 
        private const string endpoint = "tdmq.tencentcloudapi.com";
        private const string version = "2020-02-17";
-       private const string sdkVersion = "SDK_NET_3.0.1032";
+       private const string sdkVersion = "SDK_NET_3.0.1033";
 
         /// <summary>
         /// Client constructor.
