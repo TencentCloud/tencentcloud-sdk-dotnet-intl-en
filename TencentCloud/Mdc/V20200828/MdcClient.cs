051d2e4ddd1b549561a7873f00f5c363fb087c3c
tencentcloudapi <tencentcloudapi@tencent.com>
2024-12-19 05:17:15 +0800

    release 3.0.1036

--- TencentCloud/Mdc/V20200828/MdcClient.cs
+++ TencentCloud/Mdc/V20200828/MdcClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Mdc.V20200828
 
        private const string endpoint = "mdc.tencentcloudapi.com";
        private const string version = "2020-08-28";
-       private const string sdkVersion = "SDK_NET_3.0.1035";
+       private const string sdkVersion = "SDK_NET_3.0.1036";
 
         /// <summary>
         /// Client constructor.
