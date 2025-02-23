5df73d586c55cb286c4d92408c0f130670079c39
tencentcloudapi <tencentcloudapi@tencent.com>
2025-02-24 05:14:08 +0800

    release 3.0.1075

--- TencentCloud/Intlpartnersmgt/V20220928/IntlpartnersmgtClient.cs
+++ TencentCloud/Intlpartnersmgt/V20220928/IntlpartnersmgtClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Intlpartnersmgt.V20220928
 
        private const string endpoint = "intlpartnersmgt.tencentcloudapi.com";
        private const string version = "2022-09-28";
-       private const string sdkVersion = "SDK_NET_3.0.1074";
+       private const string sdkVersion = "SDK_NET_3.0.1075";
 
         /// <summary>
         /// Client constructor.
