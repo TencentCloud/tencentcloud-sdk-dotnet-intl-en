bb23785c7d95202872ba6ff500a9fb5dbb9ff361
tencentcloudapi <tencentcloudapi@tencent.com>
2025-03-19 05:31:51 +0800

    release 3.0.1092

--- TencentCloud/Dms/V20200819/DmsClient.cs
+++ TencentCloud/Dms/V20200819/DmsClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Dms.V20200819
 
        private const string endpoint = "dms.tencentcloudapi.com";
        private const string version = "2020-08-19";
-       private const string sdkVersion = "SDK_NET_3.0.1091";
+       private const string sdkVersion = "SDK_NET_3.0.1092";
 
         /// <summary>
         /// Client constructor.
