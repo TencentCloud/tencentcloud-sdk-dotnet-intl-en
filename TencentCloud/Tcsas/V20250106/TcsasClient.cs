893f0da737e7b7865bcf3cb9d2d62bb10bd980bf
tencentcloudapi <tencentcloudapi@tencent.com>
2025-03-21 05:33:52 +0800

    release 3.0.1094

--- TencentCloud/Tcsas/V20250106/TcsasClient.cs
+++ TencentCloud/Tcsas/V20250106/TcsasClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Tcsas.V20250106
 
        private const string endpoint = "tcsas.tencentcloudapi.com";
        private const string version = "2025-01-06";
-       private const string sdkVersion = "SDK_NET_3.0.1093";
+       private const string sdkVersion = "SDK_NET_3.0.1094";
 
         /// <summary>
         /// Client constructor.
