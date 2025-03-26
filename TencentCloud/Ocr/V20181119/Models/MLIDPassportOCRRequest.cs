81e45a775ab3cd6f19ddeff11f81a58bdc666210
tencentcloudapi <tencentcloudapi@tencent.com>
2025-03-27 05:43:50 +0800

    release 3.0.1098

--- TencentCloud/Ocr/V20181119/Models/MLIDPassportOCRRequest.cs
+++ TencentCloud/Ocr/V20181119/Models/MLIDPassportOCRRequest.cs
@@ -25,7 +25,7 @@ namespace TencentCloud.Ocr.V20181119.Models
     {
         
         /// <summary>
-        /// Base64-encoded value of image. The image cannot exceed 7 MB in size after being Base64-encoded. A resolution above 500x800 is recommended. PNG, JPG, JPEG, and BMP formats are supported. It is recommended that the card part occupies more than 2/3 area of the image.
+        /// Base64-encoded value of image. The image cannot exceed 7 MB in size after being Base64-encoded. A resolution above 500x800 is recommended. PNG, JPG, JPEG, BMP, and PDF formats are supported. It is recommended that the card part occupies more than 2/3 area of the image.
         /// </summary>
         [JsonProperty("ImageBase64")]
         public string ImageBase64{ get; set; }
@@ -39,7 +39,7 @@ namespace TencentCloud.Ocr.V20181119.Models
 
         /// <summary>
         /// URL address of image. (This field is not supported outside Chinese mainland)
-        /// Supported image formats: PNG, JPG, JPEG. GIF is currently not supported.
+        /// Supported image formats: PNG, JPG, JPEG, BMP, PDF.
         /// Supported image size: the downloaded image cannot exceed 7 MB after being Base64-encoded. The download time of the image cannot exceed 3 seconds.
         /// We recommend you store the image in Tencent Cloud, as a Tencent Cloud URL can guarantee higher download speed and stability.
         /// The download speed and stability of non-Tencent Cloud URLs may be low.
