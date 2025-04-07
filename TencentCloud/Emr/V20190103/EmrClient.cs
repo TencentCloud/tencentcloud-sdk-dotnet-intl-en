118ec63ea711ccc74f193b73d285de5dcff424fe
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-08 05:20:56 +0800

    release 3.0.1107

--- TencentCloud/Emr/V20190103/EmrClient.cs
+++ TencentCloud/Emr/V20190103/EmrClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Emr.V20190103
 
        private const string endpoint = "emr.intl.tencentcloudapi.com";
        private const string version = "2019-01-03";
-       private const string sdkVersion = "SDK_NET_3.0.1106";
+       private const string sdkVersion = "SDK_NET_3.0.1107";
 
         /// <summary>
         /// Client constructor.
--- TencentCloud/Intlpartnersmgt/V20220928/IntlpartnersmgtClient.cs
+++ TencentCloud/Intlpartnersmgt/V20220928/IntlpartnersmgtClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Intlpartnersmgt.V20220928
 
        private const string endpoint = "intlpartnersmgt.intl.tencentcloudapi.com";
        private const string version = "2022-09-28";
-       private const string sdkVersion = "SDK_NET_3.0.1106";
+       private const string sdkVersion = "SDK_NET_3.0.1107";
 
         /// <summary>
         /// Client constructor.
