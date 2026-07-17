8604d9398baaa368c9ea004ae86679465f256f69
tencentcloudapi <tencentcloudapi@tencent.com>
2026-07-17 12:28:27 +0800

    release 3.0.1352

--- TencentCloud/Antiddos/V20250903/AntiddosClient.cs
+++ TencentCloud/Antiddos/V20250903/AntiddosClient.cs
@@ -28,7 +28,7 @@ namespace TencentCloud.Antiddos.V20250903
 
        private const string endpoint = "antiddos.intl.tencentcloudapi.com";
        private const string version = "2025-09-03";
-       private const string sdkVersion = "SDK_NET_3.0.1350";
+       private const string sdkVersion = "SDK_NET_3.0.1352";
 
         /// <summary>
         /// Client constructor.
